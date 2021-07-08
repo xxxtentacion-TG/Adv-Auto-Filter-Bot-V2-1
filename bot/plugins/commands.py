890843e9a2ec7b0ed0b80e4a72981cc8b89acc7d
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-08 18:14:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -92,9 +92,14 @@ async def start(bot, update):
         InlineKeyboardButton("text", url="https://t.me/MGMOVIEGRAM"),
         InlineKeyboardButton("text2", url="https://t.me/MGMOVIEGRAM")
         ],[
-        InlineKeyboardButton("text3", url="https://t.me/MGMOVIEGRAM")
+        InlineKeyboardButton("text3", url="https://t.me/MGMOVIEGRAM"),
+        InlineKeyboardButton("text5", url="https://t.me/MGMOVIEGRAM")
         ],[
-        InlineKeyboardButton("text4", url="https://t.me/MGMOVIEGRAM"),
+        InlineKeyboardButton("text5", url="https://t.me/MGMOVIEGRAM"),
+        InlineKeyboardButton("text6", url="https://t.me/MGMOVIEGRAM")
+        ],[
+        InlineKeyboardButton("text5", url="https://t.me/MGMOVIEGRAM"),
+        InlineKeyboardButton("text6", url="https://t.me/MGMOVIEGRAM")
         ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
